5ab5591c23b1b57b7b016220e7e18e5fed452486
doje <giacomodandolo02@gmail.com>
2024-08-28 11:36:06 +0200

    mod

--- Strutture_Dati/Graph/Oriented/Graph.h
+++ Strutture_Dati/Graph/Oriented/Graph.h
@@ -42,43 +42,7 @@ void GRAPHmstK(Graph G);
 void GRAPHmstP(Graph G);
 
 /* CAMMINO MINIMO */
-void GRAPHspD(Graph G, int id) {
-    int v;
-    link t;
-    PQ pq = PQinit(G->V);
-    int *st, *d;
-
-    st = malloc(G->V*sizeof(int));
-    d = malloc(G->V*sizeof(int));
-    for (v = 0; v < G->V; v++) {
-        st[v] = -1;
-        d[v] = maxWT;
-        PQinsert(pq, d, v);
-    }
-
-    d[id] = 0;
-    st[id] = id;
-    PQchange(pq, d, id);
-
-    while (!PQempty(pq)) {
-        if (d[v = PQextractMin(pq, d)] != maxWT)
-            for (t = G->ladj[v]; t != G->z ; t = t->next)
-                if (d[v] + t->wt < d[t->v]) {
-                    d[t->v] = d[v] + t->wt;
-                    PQchange(pq, d, t->v);
-                    st[t->v] = v;
-                }
-    }
-
-    printf("\n Shortest path tree\n");
-    for (v = 0; v < G->V; v++)
-        printf("parent of %s is %s \n", STsearchByIndex(G->tab, v),
-                                        STsearchByIndex (G->tab, st[v]));
-    
-    printf("\n Min.dist. from %s\n", STsearchByIndex(G->tab, s));
-    for (v = 0; v < G->V; v++)
-        printf("%s: %d\n", STsearchByIndex(G->tab, v), d[v]);
-    PQfree(pq);
-}
+void GRAPHspD(Graph G, int id);
+void GRAPHspBF(Graph G, int id);
 
 #endif
